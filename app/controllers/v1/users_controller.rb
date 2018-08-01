a6b6e8e5571cd0ae0119a40262413f1841b0a371
Stefan <s.atkinson@studio-hb.com>
2018-08-01 14:01:06 +0200

    FIX specs

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -1,30 +1,29 @@
 class V1::UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
 
-  # GET /users
   def index
-    @users = User.all
+    @users = paginate User.all
 
-    paginate json: @users
+    render json: @users
   end
 
-  # GET /users/1
+  # GET /api/v1/users/1
   def show
     render json: @user
   end
 
-  # POST /users
+  # POST /api/v1/users
   def create
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user, status: :created, location: @user
+      render json: @user, status: :created, location: [:v1, @user]
     else
       render json: @user.errors, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /users/1
+  # PATCH/PUT /api/v1/users/1
   def update
     if @user.update(user_params)
       render json: @user
@@ -33,7 +32,7 @@ class V1::UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
+  # DELETE /api/v1/users/1
   def destroy
     @user.destroy
   end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Users", type: :request do
   describe "GET /api/v1/users" do
     it "works! (now write some real specs)" do
-      get users_path
+      get v1_users_path
       expect(response).to have_http_status(200)
     end
   end
