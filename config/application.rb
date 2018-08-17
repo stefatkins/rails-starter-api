7321a84a27356392145098c06dad637f3b57cc7f
Stefan <s.atkinson@studio-hb.com>
2018-08-17 13:17:21 +0200

    ADD scaffolds

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,6 @@ module RailsStarterApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.api_version = 1
   end
 end
