47372c9a4a9fbff0f0c05aee50b9632e200f3a68
Uzaif <uzaif313@gmail.com>
2018-11-09 13:35:36 +0530

    config add for revocation in jwt_blacklist model and add authentication in user api

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
-
+  before_action :authenticate_user!
   def index
     @users = User
               .includes(serialization_options[:include])
@@ -15,7 +15,6 @@ class Api::V1::UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
     if @user.save
       render json: UserSerializer.new(@user), status: :created, location: [:api, :v1, @user]
     else
